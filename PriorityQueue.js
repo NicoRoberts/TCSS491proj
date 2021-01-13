09ae2d4786b60c244c6034b2f8970a113308e8e0
Tien Tang <tientang@uw.edu>
2021-01-13 00:47:48 -0800

    finished priority queue, just needs testing

--- PriorityQueue.js
+++ PriorityQueue.js
@@ -11,7 +11,7 @@ class PriorityQueue {
         return this.entities;
     };
 
-    // Removes an entitiy to the Priority Queue
+    // Removes an entitiy from the Priority Queue
     dequeue() {
         this.swap(0, this.entities.length - 1); // swap first and last element
 
@@ -25,17 +25,19 @@ class PriorityQueue {
 
     };
 
-    // Helper method that bubbles up entities from end
+    // Helper method that sorts the heap after an element is added
     bubbleUp() {
         let index = this.entities.length - 1; // index of last inserted element
 
+        // loop while index is not 0 or element no longer needs to bubble
         while (index > 0) {
-            let parentIndex = Math.floor((index - 1) / 2); // parent index
+            let parentIndex = Math.floor((index - 1) / 2); // parent index formula
 
+            // if the priority of parent is greater than new entity, swap the two
             if (this.entities[parentIndex].priority > this.entities[index].priority) {
                 this.swap(index, parentIndex);
 
-                index = parentIndex;
+                index = parentIndex; // update index
             } else {
                 break;
             }
@@ -43,9 +45,51 @@ class PriorityQueue {
         return 0;
     };
 
-    // Helper method that readjusts the queue after dequeue
+    // Helper method that readjusts the heap after dequeue
     bubbleDown() {
-        
+        let parentIndex = 0;
+        const length = this.entities.length;
+        const entityPriority = this.entities[0].priority;
+
+        // loop breaks only if no more swaps are needed
+        while (true) {
+            // get indexes of child entities
+            let leftChildIndex = (2 * parentIndex) + 1;
+            let rightChildIndex = (2 * parentIndex) + 2;
+            let leftChildPriority, rightChildPriority;
+            let indexToSwap = null;
+
+            // if left child exists and is greater than the parent entity, plan to swap with the left child index
+            if (leftChildIndex < length){
+                leftChildPriority = this.entities[leftChildIndex].priority;
+                if (leftChildPriority < entityPriority) {
+                    indexToSwap = leftChildIndex;
+                }
+            }
+
+            // if right child exists
+            if(rightChildIndex < length) {
+                rightChildPriority = this.entities[rightChildIndex].priority;
+
+                // if right child is greater than the parent entity and there is no plans to swap yet
+                // OR if right child is greater than left child and there are plans to swap
+                if ( (rightChildPriority < entityPriority && indexToSwap === null) ||
+                     (rightChildPriority < leftChildPriority && indexToSwap !== null) ) {
+                    
+                    // plan to swap with right child index
+                    indexToSwap = rightChildIndex;
+                }
+            
+            // if no more swap are needed, break out of loop
+            if (indexToSwap === null) {
+                break;
+            }
+            
+            // swap with planned child
+            this.swap(parentIndex, indexToSwap);
+            // update starting index
+            parentIndex = indexToSwap;
+        }
     };
 
     // Helper method to swap two indexes
