2eeac83df10d3cec94f8313a4db963b087be804a
Romi Tshiorny <romi.tshiorny@gmail.com>
2021-01-14 11:18:30 -0800

    Added player animation. Fixed Diagonal Movement. Change the sprite sheet to have 1 space padding and fixed the corresponding code.

--- Player.js
+++ Player.js
@@ -1,41 +1,128 @@
 class Player{
 
-	SET_VELOCITY = {X:3, Y:2};
+	SET_VELOCITY = { X: 2, Y: 2 };
+
+	DIRECTION = {
+		RIGHT: 0,
+		LEFT: 1,
+		COUNT: 2
+	};
+	STATE = {
+		IDLE: 0,
+		WALKING: 1,
+		COUNT: 2,
+    };
 
 	constructor(game,x,y){
-		Object.assign(this, {game, x,y});
-		this.velocity = {x:0, y:0};
+		Object.assign(this, {game, x, y });
+
+		this.velocity = { x: 0, y: 0 };
+
+		this.spritesheet = ASSET_MANAGER.getAsset("./Sprites/PlayerSheet.png");
+
+		this.game.player = this;
+
+
+		this.direction = this.DIRECTION.RIGHT;
+		this.state = this.STATE.IDLE
+
+		this.animations = [];
+		this.setupCategories();
+		this.loadAnimations();
 	}
+	setupCategories() {
+	
+		for (var i = 0; i < this.STATE.COUNT; i++) {
+			this.animations.push([]);
+			for (var j = 0; j < this.DIRECTION.COUNT; j++) {
+				this.animations[i].push([]);
+            }
+		}
+
+    }
+	loadAnimations() {
+
+		this.animations[this.STATE.IDLE][this.DIRECTION.RIGHT]
+			= new Animator(this.spritesheet, 101, 1, 48, 96, 2, 0.2, 2, false, true);
+		this.animations[this.STATE.IDLE][this.DIRECTION.LEFT]
+			= new Animator(this.spritesheet, 1, 1, 48, 96, 2, 0.2, 2, false, true);
+
+		this.animations[this.STATE.WALKING][this.DIRECTION.RIGHT]
+			= new Animator(this.spritesheet, 1, 99, 48, 96, 4, 0.1, 2, false, true);
+		this.animations[this.STATE.WALKING][this.DIRECTION.LEFT]
+			= new Animator(this.spritesheet, 1, 197, 48, 96, 4, 0.1, 2, false, true);
+
+    }
 	update(){
 
 		//Update Velocity
+		var moving = false;
 		if(this.game.W){
-			this.velocity.y = -1*this.SET_VELOCITY.Y;
+			this.velocity.y = -1 * this.SET_VELOCITY.Y;
+			moving = true;
 		} else if(this.game.S){
 			this.velocity.y = this.SET_VELOCITY.Y;
+			moving = true;
 		}
 		else{
 			this.velocity.y = 0;
 		}
 
-		if(this.game.A){
+		if (this.game.A) {
+			this.direction = this.DIRECTION.LEFT;
+			moving = true;
+
 			this.velocity.x = -1*this.SET_VELOCITY.X;
-		} else if(this.game.D){
+		} else if (this.game.D) {
+			this.direction = this.DIRECTION.RIGHT
+			moving = true;
+
 			this.velocity.x = this.SET_VELOCITY.X;
 		}
-		else{
+		else {
 			this.velocity.x = 0;
 		}
 
+		this.state = (moving) ? this.STATE.WALKING : this.STATE.IDLE;
+
+		//fix velocity for diagnal movement
+
+		let diagonal = false;
+		if ((this.game.A || this.game.D) && (this.game.W || this.game.S)) {
+			this.velocity.x = (this.velocity.x / 2) * Math.sqrt(2);
+			this.velocity.y = (this.velocity.y / 2) * Math.sqrt(2);
+			diagonal = true;
+        }
+
+		if (diagonal) {
+			console.log("x speed: " + this.velocity.x
+				+ " y speed: " + this.velocity.y
+				+ " diagonal speed: " + Math.sqrt(this.velocity.y * this.velocity.y
+					+ this.velocity.x * this.velocity.x));
+
+			this.state = (moving) ? this.STATE.WALKING : this.STATE.IDLE;
+		}
+		else {
+			console.log("x speed: " + this.velocity.x
+				+ " y speed: " + this.velocity.y)
+        }
+		
+
 		//Update Position
-		this.x += this.velocity.x
-		this.y += this.velocity.y
+		this.x += this.velocity.x;
+		this.y += this.velocity.y;
+
+		if (!diagonal) {
+			this.x = Math.floor(this.x);
+			this.y = Math.floor(this.y);
+        }
 	}
 	draw(ctx){
-		ctx.fillStyle = "White";
-		ctx.strokeStyle = "Red";
-
-		ctx.strokeRect(this.x,this.y,100,100);
+		//ctx.fillStyle = "White";
+		//console.log(this.state + ":" + this.direction);
+		//ctx.strokeStyle = "Red";
+		//ctx.strokeRect(this.x, this.y, 48, 96);
+		this.animations[this.state][this.direction].drawFrame(this.game.clockTick, this.game.ctx, this.x, this.y, 1)
 	}
 
 }
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var gameEngine = new GameEngine();
 
 var ASSET_MANAGER = new AssetManager();
+ASSET_MANAGER.queueDownload("./Sprites/PlayerSheet.png");
 
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
