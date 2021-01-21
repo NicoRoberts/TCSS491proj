a2600f6a49f43af044c1801620b585d129c147e9
Romi Tshiorny <romi.tshiorny@gmail.com>
2021-01-20 17:04:28 -0800

    Remade arm rotation code to be more accurate, robust, and readible

--- Weapon.js
+++ Weapon.js
@@ -16,9 +16,12 @@ class Weapon {
 
         this.direction = this.DIRECTION.RIGHT;
 
-        //ORIGIN CENTER OF PLAYER
-        this.x = this.game.player.x + this.game.player.width;
-        this.y = this.game.player.y + this.game.player.height;
+        this.x = this.game.player.x;
+        this.y = this.game.player.y;
+
+        this.translate = { x: this.width * PARAMS.PIXELSCALER, y: this.width* PARAMS.PIXELSCALER };
+        this.armOffset = { x: this.translate.x - PARAMS.PIXELSCALER, y: this.translate.y - 5 * PARAMS.PIXELSCALER };
+        this.canvasOffset = { x: -14 * PARAMS.PIXELSCALER, y: -6 * PARAMS.PIXELSCALER };
         
     }
 
@@ -26,18 +29,24 @@ class Weapon {
         this.x = this.game.player.x;
         this.y = this.game.player.y;
 
+        let facingRight = this.game.player.direction == this.DIRECTION.RIGHT;
+
+        this.armOffset = facingRight
+            ? { x: this.translate.x - PARAMS.PIXELSCALER, y: this.translate.y - 5 * PARAMS.PIXELSCALER }
+            : { x: this.translate.x - 14 * PARAMS.PIXELSCALER, y: this.translate.y - 5 * PARAMS.PIXELSCALER };
+        this.canvasOffset = facingRight
+            ? { x: -14 * PARAMS.PIXELSCALER, y: -7 * PARAMS.PIXELSCALER }
+            : { x: -3 * PARAMS.PIXELSCALER, y: -7 * PARAMS.PIXELSCALER };
+        
         if (this.game.mouse != null) {
 
-            let xOff = this.game.player.direction == this.DIRECTION.RIGHT ? 2.5 : 12;
-            let yOff = 11;
 
-            this.source = { x: this.x + xOff * PARAMS.PIXELSCALER, y: this.y + yOff * PARAMS.PIXELSCALER };
+            this.source = { x: this.x + this.translate.x + this.canvasOffset.x + PARAMS.PIXELSCALER / 2, y: this.y + this.translate.y + this.canvasOffset.y + + PARAMS.PIXELSCALER / 2 };
             this.destination = { x: this.game.mouse.x, y: this.game.mouse.y };
             this.angle = Math.atan((this.destination.y - this.source.y) / (this.destination.x - this.source.x))
 
             this.angle = this.game.mouse.x >= this.source.x ? this.angle : this.angle + Math.PI;
             this.direction = this.game.mouse.x >= this.source.x ? this.DIRECTION.RIGHT : this.DIRECTION.LEFT;
-
             this.angle = this.game.player.direction == this.DIRECTION.LEFT? this.angle - Math.PI: this.angle; 
             
         }
@@ -57,73 +66,49 @@ class Weapon {
             var rotationCtx = rotationCanvas.getContext('2d');
             rotationCtx.save();
 
+            rotationCtx.translate(this.translate.x + PARAMS.PIXELSCALER / 2, this.translate.y + PARAMS.PIXELSCALER / 2);
+            rotationCtx.rotate(this.angle);
+            rotationCtx.translate(-1 * this.translate.x - PARAMS.PIXELSCALER / 2, -1 * this.translate.y - PARAMS.PIXELSCALER / 2);
 
-           
+            let drawOffset = this.game.player.direction == this.DIRECTION.RIGHT ? 1: 0;
+            rotationCtx.drawImage(this.spritesheet, 1 + drawOffset*(this.width * PARAMS.PIXELSCALER + 2),
+                1, this.width * PARAMS.PIXELSCALER, this.height * PARAMS.PIXELSCALER,
+                this.armOffset.x,
+                this.armOffset.y,
+                this.width * PARAMS.PIXELSCALER, this.height * PARAMS.PIXELSCALER);
 
-            //FACING RIGHT
-            if (this.game.player.direction == this.DIRECTION.RIGHT) {
-                rotationCtx.translate(1 * this.width * PARAMS.PIXELSCALER - 1 * PARAMS.PIXELSCALER, 1 * this.width * PARAMS.PIXELSCALER - 1 * PARAMS.PIXELSCALER);
-                rotationCtx.rotate(this.angle);
-                rotationCtx.translate(-1 * this.width * PARAMS.PIXELSCALER  -1 * PARAMS.PIXELSCALER, -1 * this.width * PARAMS.PIXELSCALER + 1 * PARAMS.PIXELSCALER);
-
-                rotationCtx.drawImage(this.spritesheet, 1 + this.width * PARAMS.PIXELSCALER + 2,
-                    1, this.width * PARAMS.PIXELSCALER, this.height * PARAMS.PIXELSCALER,
-                    this.width * PARAMS.PIXELSCALER - 1 * PARAMS.PIXELSCALER,
-                    this.width * PARAMS.PIXELSCALER - 6 * PARAMS.PIXELSCALER,
+            if (PARAMS.DEBUG) {
+                rotationCtx.strokeStyle = 'Red';
+                rotationCtx.strokeRect(this.armOffset.x, this.armOffset.y,
                     this.width * PARAMS.PIXELSCALER, this.height * PARAMS.PIXELSCALER);
 
-                if (PARAMS.DEBUG) {
-                    rotationCtx.strokeStyle = 'Red';
-                    rotationCtx.strokeRect(this.width * PARAMS.PIXELSCALER - PARAMS.PIXELSCALER, this.width * PARAMS.PIXELSCALER - 6 * PARAMS.PIXELSCALER,
-                        this.width * PARAMS.PIXELSCALER, this.height * PARAMS.PIXELSCALER);
+                rotationCtx.fillStyle = "Green";
+                rotationCtx.fillRect(this.translate.x, this.translate.y, PARAMS.PIXELSCALER, PARAMS.PIXELSCALER);
 
-                   // rotationCtx.strokeStyle = 'Orange';
-                   // rotationCtx.strokeRect(0, 0,
-                   //     this.width * PARAMS.PIXELSCALER * 2,
-                   //     this.width * PARAMS.PIXELSCALER * 2);
-                }
+                //rotationCtx.strokeStyle = 'Orange';
+                //rotationCtx.strokeRect(0, 0,
+                //    this.width * PARAMS.PIXELSCALER * 2,
+                //    this.width * PARAMS.PIXELSCALER * 2);
             }
-            //FACING LEFT
-            else {
-                rotationCtx.translate(1 * this.width * PARAMS.PIXELSCALER - 1 * PARAMS.PIXELSCALER, 1 * this.width * PARAMS.PIXELSCALER - 1 * PARAMS.PIXELSCALER);
-                rotationCtx.rotate(this.angle);
-                rotationCtx.translate(-1 * this.width * PARAMS.PIXELSCALER + 1 * PARAMS.PIXELSCALER, -1 * this.width * PARAMS.PIXELSCALER + 1 * PARAMS.PIXELSCALER);
-
-                rotationCtx.drawImage(this.spritesheet, 1 ,
-                    1, this.width * PARAMS.PIXELSCALER, this.height * PARAMS.PIXELSCALER,
-                    1 * PARAMS.PIXELSCALER,
-                    this.width * PARAMS.PIXELSCALER - 6 * PARAMS.PIXELSCALER,
-                    this.width * PARAMS.PIXELSCALER, this.height * PARAMS.PIXELSCALER);
-
-                if (PARAMS.DEBUG) {
-                    rotationCtx.strokeStyle = 'Red';
-                    rotationCtx.strokeRect(1 * PARAMS.PIXELSCALER, this.width * PARAMS.PIXELSCALER - 6 * PARAMS.PIXELSCALER,
-                        this.width * PARAMS.PIXELSCALER, this.height * PARAMS.PIXELSCALER);
-
-                   // rotationCtx.strokeStyle = 'Orange';
-                   // rotationCtx.strokeRect(0, 0,
-                   //     this.width * PARAMS.PIXELSCALER * 2,
-                   //     this.width * PARAMS.PIXELSCALER * 2);
-                }
-            }
-
 
             rotationCtx.restore();
 
-            let xOff = this.game.player.direction == this.DIRECTION.RIGHT ? this.width * PARAMS.PIXELSCALER - 12* PARAMS.PIXELSCALER : PARAMS.PIXELSCALER;
-            let yOff = 5;
-
-            ctx.drawImage(rotationCanvas, this.x - xOff * PARAMS.PIXELSCALER, this.y - yOff* PARAMS.PIXELSCALER,
+            ctx.drawImage(rotationCanvas, this.x + this.canvasOffset.x, this.y + this.canvasOffset.y,
                 this.width * PARAMS.PIXELSCALER * 2, this.width * PARAMS.PIXELSCALER * 2);
 
 
             if (PARAMS.DEBUG) {
-                xOff = this.game.player.direction == this.DIRECTION.RIGHT ? 2.5 : 12;
-                yOff = 11;
+
+                let lineScaler = this.game.player.direction == this.DIRECTION.RIGHT ? 0.9 : -0.9;
+
+                let angleOffset = {
+                    x: lineScaler * this.width * PARAMS.PIXELSCALER * Math.cos(this.angle),
+                    y: lineScaler * this.width * PARAMS.PIXELSCALER * Math.sin(this.angle)
+                };
 
                 ctx.strokeStyle = this.game.click ? "Blue" : "Red";
                 ctx.beginPath();
-                ctx.moveTo(this.x + xOff * PARAMS.PIXELSCALER, this.y + yOff * PARAMS.PIXELSCALER - PARAMS.PIXELSCALER);
+                ctx.moveTo(this.source.x + angleOffset.x, this.source.y + angleOffset.y);
                 ctx.lineTo(this.destination.x, this.destination.y);
                 ctx.stroke();
             }
