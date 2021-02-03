c984a84bd0346189ac7bc2ecb48a7db9095d6874
Tien Tang <tientang@uw.edu>
2021-02-02 23:26:05 -0800

    glowing shards

--- Shards.js
+++ Shards.js
@@ -4,9 +4,10 @@ class Shards {
         Object.assign(this, {game, x, y});
 
         this.spritesheet = ASSET_MANAGER.getAsset("./Sprites/Shard.png");
-
-        this.width = 32;
-        this.height = 32;
+        
+        this.scale = 2;
+        this.width = 18;
+        this.height = 18;
 
         // do later: implement a way to randomize shard location, ensure that shard is not spawned near player at start of a level
         this.positionx = 0;
@@ -14,9 +15,9 @@ class Shards {
 
         this.priority = 2;
 
-        this.shard = new Animator(this.spritesheet, 0, 0, this.width, this.height, 1, 1, 1, false, true);
+        this.shard = new Animator(this.spritesheet, 0, 0, this.width, this.height, 6, 0.2, 0, false, true);
         
-        this.hitbox = new HitBox(this, this.width, this.height);
+        this.hitbox = new HitBox(this, this.width * this.scale, this.height * this.scale);
     
     };
 
@@ -33,7 +34,7 @@ class Shards {
         if (PARAMS.DEBUG) {
             this.hitbox.draw(ctx);
         }
-        this.shard.drawFrame(this.game.clockTick, this.game.ctx, this.positionx, this.positiony, 1);
+        this.shard.drawFrame(this.game.clockTick, this.game.ctx, this.positionx, this.positiony, 1 * this.scale);
 
     };
 };
