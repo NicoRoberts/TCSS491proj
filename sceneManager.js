986222ff01808c76cfdd66b2cdfb52efdbda4ada
Tien Tang <tientang@uw.edu>
2021-02-21 19:17:55 -0800

    removed coins

--- sceneManager.js
+++ sceneManager.js
@@ -183,15 +183,15 @@ class SceneManager {
 		
 
 		//spawning coins to test shop system
-		for(var k = 0; k < 50; k++){
-			let open = this.grid.getNonClosedGrids();
-			if (open.length <= 0) {
-				break;
-			}
-			let randomIndex = randomInt(open.length);
-			let coin = new Coin(this.game, open[randomIndex].x, open[randomIndex].y);
-			open[randomIndex].addTerrain(coin);
-		}
+		// for(var k = 0; k < 50; k++){
+		// 	let open = this.grid.getNonClosedGrids();
+		// 	if (open.length <= 0) {
+		// 		break;
+		// 	}
+		// 	let randomIndex = randomInt(open.length);
+		// 	let coin = new Coin(this.game, open[randomIndex].x, open[randomIndex].y);
+		// 	open[randomIndex].addTerrain(coin);
+		// }
 		
 
 		// testing map generation
@@ -263,7 +263,7 @@ class SceneManager {
 		   }
 		}
 
-		// gangway testing
+		// spawn gangway .2 seconds after loading yacht level so that player does not get forced into next stage
 		if (this.game.stage == "yacht") {
 			this.timeInYacht += this.game.clockTick;
 			if (this.timeInYacht > .2 && !this.gangwaySpawned) {
